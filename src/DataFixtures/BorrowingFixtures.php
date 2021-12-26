a1c4a18ad07276cf102b80cef0352af246681c02
assane <assane.diouf@centrale-marseille.com>
2021-12-26 13:03:56 +0100

    modification fixtures borrowing

--- src/DataFixtures/BorrowingFixtures.php
+++ src/DataFixtures/BorrowingFixtures.php
@@ -12,17 +12,6 @@ class BorrowingFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
-        $userRepo = $this->getDoctrine()->getRository(User::class);
-        $equipmentRepo = $this->getDoctrine()->getRepository(Equipment::class);
-
-        $emprunt1 = new Borrowing();
-        $emprunt1->setEquipment($equipmentRepo->findByName("Chargeurs de téléphone"));
-        $emprunt1->setBorrowedBy($userRepo->findByUsername("MrLambda"));
-        $emprunt1->setLendBy($userRepo->findByUsername("LeBertNoel"));
-        $emprunt1->setAllowadDays(1);
-        $emprunt1->setStartedOn(new \DateTime());
-        $emprunt1->setEndedOn(new \DateTime());
-        $manager->persist($emprunt1);
 
         $manager->flush();
     }
